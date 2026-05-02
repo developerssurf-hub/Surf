0bdad1a1dc097f19417445e388c287940e4b339d
JuanToSeidor <jdtorres@seidoranalytics.com>
2026-05-01 21:18:23 -0300

    feat: Envíos a todo el país

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -6,6 +6,7 @@ export default function Hero() {
       <div className='container bg-[url(/hero/bg.png)] bg-no-repeat bg-contain bg-center w-full h-full flex justify-center relative'>
         <img src="/hero/campera.png" className='max-h-[200px] lg:max-h-[550px] md:max-h-[400px] sm:max-h-[350px] ' alt="" />
         <img src="/hero/over.png" className='absolute top-[50%] right-[5%] translate-y-[-50%] w-[100px] lg:w-[450px] md:w-[300px] sm:w-[200px] ' alt="" />
+        <img src="/hero/eatelp.png" className="absolute w-[100px] md:w-[140px] top-[50%] left-[15%] md:left-[25%] translate-y-[-50%] -rotate-25" alt="" srcset="" />
       </div>
       <h1 className="title">
         Cuidamos todos los detalles
